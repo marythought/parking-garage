636f9ebb97234a060d07be7d15087d7e6f5974e0
Mary Dickson Diaz <mdickson@gmail.com>
2015-09-13 20:40:20 -0700

    initial commit

---
+++ spec/features/parking_garage_spec.rb
@@ -0,0 +1,14 @@
+require "spec_helper"
+require "parking_garage.rb"
+
+describe 'parking garage' do
+  # it "returns a valid result" do
+  #   assert_equal project(0), 0
+  # end
+
+  # it "returns an error with a negative number" do
+  #   proc { project(-1) }.must_raise ArgumentError
+  # end
+end
+
+
